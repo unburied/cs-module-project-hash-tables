4259d9219d207fe084cd13bfb12eef485cc8f372
David <davi86m@gmail.com>
2020-09-08 23:14:50 -0400

    expensive done

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,32 @@
 # Your code here
 
 
-def expensive_seq(x, y, z):
-    # Your code here
+def expensive_seq(x, y, z, cache = {}):
+    # set key as a tuple
+    key = (x,y,z)
 
+    # check if in dict
+    if key in cache:
+        return cache[key]
+
+    # base case
+    if x <= 0:
+        return y + z
+    else:
+        #find val for this tuple
+        val = (
+            expensive_seq(x-1,y+1,z, cache) +
+            expensive_seq(x-2,y+2,z*2, cache) + 
+            expensive_seq(x-3,y+3,z*3, cache)
+        )
+
+        # store this val in dict
+        cache[key] = val
+
+        # send val upstream
+        return val
+
+    
 
 
 if __name__ == "__main__":
